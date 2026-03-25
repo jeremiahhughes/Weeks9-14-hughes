e662183cd9ec749a90f6dc8190c7879e12fe8498
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-03-25 09:42:17 -0400

    Added sound effects to the animation events for the knight

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    public AudioSource sfx;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Footstep()
+    {
+        //Debug.Log("Step!");
+        sfx.Play();
+    }
+}
