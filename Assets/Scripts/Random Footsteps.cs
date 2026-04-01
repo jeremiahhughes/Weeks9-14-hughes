079d16a97ffb5d5a4902a058d2548f4e28c29a3c
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-04-01 10:11:19 -0400

    did the Kaiju challenge for follwoing the knight making a tileset and also camera tracking movement.

--- Assets/Scripts/Random Footsteps.cs
+++ Assets/Scripts/Random Footsteps.cs
@@ -1,13 +1,20 @@
 using NUnit.Framework;
+using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 
 public class RandomFootsteps : MonoBehaviour
 {
     public AudioSource sfxSource;
     public AudioClip[] footsteps;
     public ParticleSystem particles;
+    public float speed = 5f;
+    public Vector2 movement;
+    public Vector2 direction;
+    public CinemachineImpulseSource ImpulseSource;
+
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,12 +26,24 @@ public class RandomFootsteps : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        transform.eulerAngles = (Vector3)direction * speed * Time.deltaTime;
     }
 
     public void RandoFootsteps()
     {
         sfxSource.clip = footsteps[Random.Range(0,footsteps.Length)];
         sfxSource.Play();
+        ImpulseSource.GenerateImpulse();
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    //public void OnDirection(InputAction.CallbackContext context)
+    //{
+    //    direction = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    //}
 }
