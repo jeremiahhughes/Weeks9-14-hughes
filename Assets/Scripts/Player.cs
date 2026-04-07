52bffaf3bb43d392d03f77e6f2c97340adb5c429
jeremiahhughes <148461474+jeremiahhughes@users.noreply.github.com>
2026-04-07 19:48:14 -0400

    updated the player script to track right stick movment to move the crosshair on the screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour
 {
     public float speed = 5;
     public Vector2 movement;
+    public Vector2 aimDirection;
+    public Transform crosshair;
+    public float crosshairDistance = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +17,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Handles Movment
         transform.position += (Vector3)movement * speed * Time.deltaTime;
+        // Handles Aim Input
+        if (crosshair != null)
+        {
+            crosshair.position = transform.position + new Vector3(aimDirection.x, aimDirection.y, 0) * crosshairDistance;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 stickInput = Gamepad.current.rightStick.ReadValue();
+        if (stickInput.magnitude > 0.1f)
+        {
+            aimDirection = stickInput;
+        }
     }
 }
